1d8f4933482da512ffc6561e3605c7088c4feeb6
Nivaldo Gonçalves Caixeta Junior <67606471+Nivaldotk@users.noreply.github.com>
2021-10-21 09:22:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,38 @@
+import os
+
+from flask import Flask, request
+
 import telebot
 
-CHAVE_API = "1980584583:AAEL6-Psse_gwUZqEp3t1I_VsF_3zgG_84g"
-
-bot = telebot.TeleBot(CHAVE_API)
-
-@bot.message_handler(commands=['animes'])
-def animes(mensagem):
-    bot.send_message(mensagem.chat.id, "Os animes do qual eu mais gosto são Berserk, Sankarea e D-gray man e você?")
-
-@bot.message_handler(commands=['jogos'])
-def jogos(mensagem):
-    bot.send_message(mensagem.chat.id, "Meu jogo favorito é Resident Evil, e adoro jogar YU GI OH, qual jogos vc gosta de jogar?")
-
-@bot.message_handler(commands=['sobreVida'])
-def sobreVida(mensagem):
-    bot.send_message(mensagem.chat.id, "Ainda não tenho uma vida completa mas logo terei ;--;")
-
-@bot.message_handler(commands=['opcao1'])
-def opcao1(mensagem):
-    texto = """Sobre o que você deseja conversar(clique em uma opção?
-    /animes Animes
-    /jogos Jogos
-    /sobreVida SobreVida"""
-    bot.send_message(mensagem.chat.id, texto)
-
-@bot.message_handler(commands=['opcao2'])
-def opcao2(mensagem):
-    bot.send_message(mensagem.chat.id, "Desculpa ainda não tenho autonomia para interpretar uma frase sozinha, mas muito obrigada, pela gentileza ela esta salva no meu banco de dados")
-
-@bot.message_handler(commands=['opcao3'])
-def opcao3(mensagem):
-    print(mensagem)
-    bot.send_message(mensagem.chat.id, "Muito obrigado! Junior mandou um abraço de volta!")
+TOKEN = "1980584583:AAEL6-Psse_gwUZqEp3t1I_VsF_3zgG_84g"
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
-def verificar(mensagem):
-    return True
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-@bot.message_handler(func=verificar)
-def responder(mensagem):
-    texto = """Olá eu sou a Yui, no que posso te ajudar?
-    /opcao1 Conversar comigo
-    /opcao2 Me mandar alguma frase
-    /opcao3 Mandar um abraço para o Junior
- Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
-    bot.reply_to(mensagem,texto)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
-bot.polling()
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
